3401ded819ecc78775c53f614b31873a74f76022
xxvss <62797681+xxvss@users.noreply.github.com>
2023-04-14 18:50:53 +0800

    Add files via upload

---
+++ route/setting.php
@@ -0,0 +1,135 @@
+<?php
+
+!defined('DEBUG') AND exit('Access Denied.');
+
+$action = param(1);
+
+include _include(APP_PATH.'model/smtp.func.php');
+$smtplist = smtp_init(APP_PATH.'conf/smtp.conf.php');
+// hook admin_setting_start.php
+
+if($action == 'base') {
+	
+	// hook admin_setting_base_get_post.php
+	
+	if($method == 'GET') {
+		
+		// hook admin_setting_base_get_start.php
+		
+		$input = array();
+		$input['sitename'] = form_text('sitename', $conf['sitename']);
+		$input['sitebrief'] = form_textarea('sitebrief', $conf['sitebrief'], '100%', 100);
+		$input['runlevel'] = form_radio('runlevel', array(0=>lang('runlevel_0'), 1=>lang('runlevel_1'), 2=>lang('runlevel_2'), 3=>lang('runlevel_3'), 4=>lang('runlevel_4'), 5=>lang('runlevel_5')), $conf['runlevel']);
+		$input['user_create_on'] = form_radio_yes_no('user_create_on', $conf['user_create_on']);
+		$input['user_create_email_on'] = form_radio_yes_no('user_create_email_on', $conf['user_create_email_on']);
+		$input['user_resetpw_on'] = form_radio_yes_no('user_resetpw_on', $conf['user_resetpw_on']);
+		$input['lang'] = form_select('lang', array('zh-cn'=>lang('lang_zh_cn'), 'zh-tw'=>lang('lang_zh_tw'), 'en-us'=>lang('lang_en_us'), 'ru-ru'=>lang('lang_ru_ru'), 'th-th'=>lang('lang_th_th')), $conf['lang']);
+		$input['url_rewrite_on'] = form_radio_yes_no('url_rewrite_on', $conf['url_rewrite_on']); 
+		$input['cdn_on'] = form_radio_yes_no('cdn_on', $conf['cdn_on']); 
+		$input['pagesize'] = form_text('pagesize', $conf['pagesize'], 100); 
+		$input['postlist_pagesize'] = form_text('postlist_pagesize', $conf['postlist_pagesize'], 100); 
+
+		$input['logo_pc_url'] = form_text('logo_pc_url', $conf['logo_pc_url']);
+		$input['logo_mobile_url'] = form_text('logo_mobile_url', $conf['logo_mobile_url']);
+		$input['footer_left_html'] = form_textarea('footer_left_html', $conf['footer_left_html'], '100%', 100);
+		$input['footer_right_html'] = form_textarea('footer_right_html', $conf['footer_right_html'], '100%', 100);
+		$header['title'] = lang('admin_site_setting');
+		$header['mobile_title'] =lang('admin_site_setting');
+		
+		// hook admin_setting_base_get_end.php
+		
+		include _include(ADMIN_PATH.'view/htm/setting_base.htm');
+		
+	} else {
+		
+		$sitebrief = param('sitebrief', '', FALSE);
+		$sitename = param('sitename', '', FALSE);
+		$runlevel = param('runlevel', 0);
+		$user_create_on = param('user_create_on', 0);
+		$user_create_email_on = param('user_create_email_on', 0);
+		$user_resetpw_on = param('user_resetpw_on', 0);
+		$url_rewrite_on = param('url_rewrite_on', 0); 
+		$cdn_on = param('cdn_on', 0); 
+		$pagesize = param('pagesize', 0); 
+		$postlist_pagesize = param('postlist_pagesize', 0); 
+		$logo_mobile_url = param('logo_mobile_url', '',FALSE);  	
+		$logo_pc_url = param('logo_pc_url', '',FALSE);  	
+		$footer_left_html = param('footer_left_html', '',FALSE);  	
+		$footer_right_html = param('footer_right_html', '',FALSE);  	 
+		$_lang = param('lang');
+		
+		// hook admin_setting_base_post_start.php
+		
+		$replace = array();
+		$replace['sitename'] = $sitename;
+		$replace['sitebrief'] = $sitebrief;
+		$replace['runlevel'] = $runlevel;
+		$replace['user_create_on'] = $user_create_on;
+		$replace['user_create_email_on'] = $user_create_email_on;
+		$replace['user_resetpw_on'] = $user_resetpw_on;
+		$replace['lang'] = $_lang;
+		$replace['url_rewrite_on'] = $url_rewrite_on;
+		$replace['cdn_on'] = $cdn_on;
+		$replace['pagesize'] = $pagesize;
+		$replace['postlist_pagesize'] = $postlist_pagesize; 	
+		$replace['logo_mobile_url'] = $logo_mobile_url;	
+		$replace['logo_pc_url'] = $logo_pc_url;	
+		$replace['footer_left_html'] = $footer_left_html;	
+		$replace['footer_right_html'] = $footer_right_html;	
+		file_replace_var(APP_PATH.'conf/conf.php', $replace);
+	
+		// hook admin_setting_base_post_end.php
+		
+		message(0, lang('modify_successfully'));
+	}
+
+} elseif($action == 'smtp') {
+
+	// hook admin_setting_smtp_get_post.php
+	
+	if($method == 'GET') {
+		
+		// hook admin_setting_smtp_get_start.php
+		
+		$header['title'] = lang('admin_setting_smtp');
+		$header['mobile_title'] = lang('admin_setting_smtp');
+	
+		$smtplist = smtp_find();
+		$maxid = smtp_maxid();
+		
+		// hook admin_setting_smtp_get_end.php
+		
+		include _include(ADMIN_PATH."view/htm/setting_smtp.htm");
+	
+	} else {
+		
+		// hook admin_setting_smtp_post_start.php
+		
+		$email = param('email', array(''));
+		$host = param('host', array(''));
+		$port = param('port', array(0));
+		$user = param('user', array(''));
+		$pass = param('pass', array(''));
+		
+		$smtplist = array();
+		foreach ($email as $k=>$v) {
+			$smtplist[$k] = array(
+				'email'=>$email[$k],
+				'host'=>$host[$k],
+				'port'=>$port[$k],
+				'user'=>$user[$k],
+				'pass'=>$pass[$k],
+			);
+		}
+		$r = file_put_contents_try(APP_PATH.'conf/smtp.conf.php', "<?php\r\nreturn ".var_export($smtplist,true).";\r\n?>");
+		!$r AND message(-1, lang('conf/smtp.conf.php', array('file'=>'conf/smtp.conf.php')));
+		
+		// hook admin_setting_smtp_post_end.php
+		
+		message(0, lang('save_successfully'));
+	}
+}
+
+// hook admin_setting_end.php
+
+?>
